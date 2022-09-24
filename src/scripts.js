5b984d8c080fc3736250d49169882397565da671
Evan Swanson <evan.sundelius@gmail.com>
2022-09-23 21:36:31 -0500

    Complete CSS first draft

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,15 @@
 //Initial commit
+var switchButton = document.querySelector('.switch-button')
+var classicContainer = document.querySelector('.classic-container')
+var elementalContainer = document.querySelector('.elemental-container')
+var classicDiagram = document.querySelector('.classic-diagram')
+var elementalDiagram = document.querySelector('.elemental-diagram')
+
+switchButton.addEventListener('click', switchMode)
+
+function switchMode() {
+  classicContainer.classList.toggle('hidden')
+  elementalContainer.classList.toggle('hidden')
+  classicDiagram.classList.toggle('hidden')
+  elementalDiagram.classList.toggle('hidden')
+}
